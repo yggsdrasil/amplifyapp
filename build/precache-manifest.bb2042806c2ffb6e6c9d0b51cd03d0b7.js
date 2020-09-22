2858850157af449a04a9b98353208c79ae712194
yggsdrasil <67926841+yggsdrasil@users.noreply.github.com>
2020-09-22 16:01:39 -0400

    Add files via upload

---
+++ build/precache-manifest.bb2042806c2ffb6e6c9d0b51cd03d0b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5bcd1d64f78eb75b14439d3e1edb9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd3d63e402bdc8565927",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7680ca4e52dd2990b538",
+    "url": "/static/js/2.e9f12fd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9f12fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3d63e402bdc8565927",
+    "url": "/static/js/main.91438e46.chunk.js"
+  },
+  {
+    "revision": "a74fc630eaa204d00694",
+    "url": "/static/js/runtime-main.e5cde8aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
